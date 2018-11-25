2a165874242fd4354f282f303ca286eded582c2a
rafalopes88 <rafalopes88@gmail.com>
2018-11-24 23:40:21 -0200

    coisas

---
+++ Assets/scripts/Camera/TrackPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackPlayer : MonoBehaviour
+{
+
+	public GameObject player;       //Public variable to store a reference to the player game object
+
+
+	private float offset;         //Private variable to store the offset distance between the player and camera
+
+	Vector3 temp;
+
+	// Use this for initialization
+	void Start()
+	{
+		//Calculate and store the offset value by getting the distance between the player's position and camera's position.
+		offset = transform.position.x - player.transform.position.x;
+	}
+
+	// LateUpdate is called after Update each frame
+	void LateUpdate()
+	{
+		// Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+		if((player.transform.position.x + offset) > 253 && (player.transform.position.x + offset) < 440) {
+			temp = new Vector3(player.transform.position.x + offset, transform.position.y, transform.position.z);
+			transform.position = temp;
+		}
+	}
+}
--- Assets/scripts/Gato/SamuraiController.cs
+++ Assets/scripts/Gato/SamuraiController.cs
@@ -15,7 +15,9 @@ public class SamuraiController : MonoBehaviour {
 	public LayerMask whatIsGround, whatIsArrandor;
     public float jumpForce = 7000f;
     public float run = 2f;
+	public bool distracted;
     
+	public float targetPosition;
 
     // Use this for initialization
     void Awake () {
@@ -27,34 +29,48 @@ public class SamuraiController : MonoBehaviour {
 
         grounded = Physics2D.OverlapCircle(groundCheck.position, radius, whatIsGround);
 		escalando = Physics2D.OverlapCircle(groundCheck.position, radius, whatIsArrandor);
-		Debug.Log (grounded);
+
+
         anim.SetBool("ground", grounded);
         anim.SetFloat("vSpeed", rb.velocity.y);
 
         float move;
 
-        if (Input.GetButton("Run"))
-            move = Input.GetAxis("Horizontal") * run;
-        else
-            move = Input.GetAxis("Horizontal");
+		if (!distracted) {
+			move = Input.GetAxis ("Horizontal");
+			anim.SetFloat("speed", Mathf.Abs(move));
+			rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
 
-        anim.SetFloat("speed", Mathf.Abs(move));
-        rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
-
-        if (move > 0 && !facingRight)
-            Flip();
-        else if(move < 0 && facingRight)
-            Flip();
+			if (move > 0 && !facingRight)
+				Flip();
+			else if(move < 0 && facingRight)
+				Flip();
+		}
     }
 
     void Update()
     {
-		if((grounded || escalando) && Input.GetButtonDown("Jump"))
+		if((grounded || escalando) && Input.GetButtonDown("Jump") && !distracted)
         {
             anim.SetBool("ground", false);
             rb.AddForce(new Vector2(0,jumpForce));
         }
     }
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		distracted = true;
+		anim.SetBool("distracted", true );
+		StartCoroutine (SaiDoSusto());
+	}
+
+	IEnumerator SaiDoSusto()
+	{
+
+		yield return new WaitForSeconds(3);
+		distracted = false ;
+		anim.SetBool("distracted", false );
+	}
 
     void Flip()
     {
